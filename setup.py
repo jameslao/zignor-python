a00780dc1eb1f6518611feb6d739be211b295646
James Lao <james.lao@gmail.com>
2015-11-13 17:22:35 -0800

    update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ _zignor = Extension('_zignor',
                     extra_compile_args=['-O3'])
 
 setup(name='zignor',
-      version='0.14',
+      version='0.15',
       description='Zignor RNG on Python',
       author='James Lao',
       author_email='james@jlao.net',
